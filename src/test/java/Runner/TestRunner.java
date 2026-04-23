349c31e0c12de113286eb1a4495588eac7c1a52c
Subhash <163117768+SUBHASH200505@users.noreply.github.com>
2026-04-23 12:16:06 +0530

    creating common file

---
+++ src/test/java/Runner/TestRunner.java
@@ -0,0 +1,17 @@
+package Runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"stepdefinitions", "hooks"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber-report.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+        },
+        monochrome = true
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+}
