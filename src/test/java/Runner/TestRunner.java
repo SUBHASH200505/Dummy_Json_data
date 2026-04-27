fb1b9b5fd0ae4393bf761214ac9b9679fb48af94
Subhash <163117768+SUBHASH200505@users.noreply.github.com>
2026-04-27 09:08:31 +0530

    updated auth test runner

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/features/Auth.feature",
+        features = "src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
         plugin = {
                 "pretty",
